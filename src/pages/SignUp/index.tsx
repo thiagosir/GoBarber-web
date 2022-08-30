c0ab83d37d3a8e979a77905e937c4b5a5d39400d
thiagosir <thiagobento9@hotmail.com>
2022-08-30 09:45:35 -0300

    Isolando componentes e adicionando tela de cadastro

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,36 +1,35 @@
 import React from 'react';
-import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
+import { FiArrowLeft, FiMail, FiUser, FiLock } from "react-icons/fi";
 import logoImg from '../../assets/logo.svg'
 import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignIn: React.FC = () => (
+const SignUp: React.FC = () => (
 <Container>
+<Background/>
 <Content>
     <img src={logoImg} alt="GoBarber" />
 
     <form action="">
-        <h1>Faça seu logon</h1>
+        <h1>Faça seu cadastro</h1>
 
+        <Input name="name" icon={FiUser} placeholder='Nome'/>
         <Input name="email" icon={FiMail} placeholder='E-mail'/>
         <Input name="password" icon={FiLock} placeholder='Senha'/>
 
 
-        <Button type='submit'>Entrar</Button>
-
-        <a href="forgot">Esqueci minha senha</a>
+        <Button type='submit'>Cadastrar</Button>
 
     </form>
-    <a href="login">
-        <FiLogIn/>
-        Criar Conta
+    <a href="">
+        <FiArrowLeft/>
+        Voltar para logon
     </a>
 
 
 </Content>
-<Background/>
 </Container>
 );
 
-export default SignIn
+export default SignUp
