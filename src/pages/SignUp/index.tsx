74d4050d3d9ed45fa0f5a30aec5dec2e013e2905
thiagosir <thiagobento9@hotmail.com>
2022-09-09 15:20:01 -0300

    Adicionando Unforms ao projeto

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,35 +1,38 @@
-import React from 'react';
+import React from "react";
 import { FiArrowLeft, FiMail, FiUser, FiLock } from "react-icons/fi";
-import logoImg from '../../assets/logo.svg'
-import { Container, Content, Background } from './styles';
-import Input from '../../components/Input';
-import Button from '../../components/Button';
+import { Form } from "@unform/web";
+import logoImg from "../../assets/logo.svg";
+import { Container, Content, Background } from "./styles";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
 
-const SignUp: React.FC = () => (
-<Container>
-<Background/>
-<Content>
-    <img src={logoImg} alt="GoBarber" />
+const SignUp: React.FC = () => {
+    function handleSubmit(data: object):void {
+        console.log(data);
+    }
 
-    <form action="">
-        <h1>Faça seu cadastro</h1>
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
 
-        <Input name="name" icon={FiUser} placeholder='Nome'/>
-        <Input name="email" icon={FiMail} placeholder='E-mail'/>
-        <Input name="password" icon={FiLock} placeholder='Senha'/>
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
 
+          <Input name="name" type="text" icon={FiUser} placeholder="Nome" />
+          <Input name="email" type="email" icon={FiMail} placeholder="E-mail" />
+          <Input name="password" type="password" icon={FiLock} placeholder="Senha" />
 
-        <Button type='submit'>Cadastrar</Button>
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
-    </form>
-    <a href="">
-        <FiArrowLeft/>
-        Voltar para logon
-    </a>
-
-
-</Content>
-</Container>
-);
-
-export default SignUp
+export default SignUp;
